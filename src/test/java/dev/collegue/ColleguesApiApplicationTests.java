5aae906cc23c8ef9e832c5ad83472c88610b45ea
Helvin <hjwc86@gmail.com>
2020-09-23 15:29:39 +0200

    Initialisation projet

---
+++ src/test/java/dev/collegue/ColleguesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.collegue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColleguesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
